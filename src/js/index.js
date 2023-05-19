7e379cbd464af1841a254e4568ff73355c8b06ea
and-sheera <shirnin.andrew@gmail.com>
2023-05-19 21:37:13 +0300

    upd

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,9 @@
 import '../styles/style.scss'
 import 'virtual:svg-icons-register'
 import 'lazysizes'
-// import uiDatepicker from '../blocks/_ui/ui-datepicker/ui-datepicker'
-// import uiInput from '../blocks/_ui/ui-input/ui-input'
-// import uiRange from '../blocks/_ui/ui-range/ui-range'
-// import uiSelect from '../blocks/_ui/ui-select/ui-select'
-// import uiTextarea from '../blocks/_ui/ui-textarea/ui-textarea'
-// import uiTooltip from '../blocks/_ui/ui-tooltip/ui-tooltip'
-// import gallery from '../blocks/gallery/gallery'
-// import sliderInit from '../blocks/slider/slider'
-// import tabs from '../blocks/tabs/tabs'
+
 import { ChartMaster } from 'chart-master'
-document.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('load', function () {
   const s1 = ChartMaster.createChart('pie', {
     element: document.querySelector('#chart1'),
     data: [
